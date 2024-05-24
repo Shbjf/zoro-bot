6e71bf07615abb75c3df4b30035d0bfce3bffe83
Shbjf <136927197+Shbjf@users.noreply.github.com>
2024-05-24 19:43:05 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -3,21 +3,19 @@ import { join } from 'path'
 import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 //import { plugins } from '../lib/plugins.js'
-let tags = {
-  'morocco':'  ‎أوامر للمغاربة',
+let tags ={
   'applications':'‎ أوامر التطبيقات‎',
   'drawing':'‎ توليد الصور‎ أوامر' ,
   'ai':'‎ الذكاء الاصطناعي‎ أوامر',
   'infobot':'‎ معلومات البوت‎',
   'downloader':'‎ أوامر التحميلات',
-  'anime':'‎ أوامر عن  الأنيم',
+  'anime':'‎ أوامر عن  الأنمي',
   'islam':'‎ الدين هو الاسلام‎',
   'owner':'‎ اوامر صاحب البوت',
   'search':'‎ أوامر البحث',
   'audio-changer':'‎ تعديل الصوتيات‎',
   'sticker':'‎ أوامر الملصقات',
   'image-edit':'‎ تعديل الصور',
-  'pdf':'‎ pdf ومشتقاته‎',
   'uploader':'‎‎ رفع الملفات‎',
 }
 const defaultMenu = {
@@ -28,12 +26,11 @@ const defaultMenu = {
 ⏰ *Uptime:* %muptime  
 ┗━━━━━━━━━━⬣
 %readmore
-  ≡ *B O B I Z A | M E N U*
+  ≡ *𝒁 𝑶 𝑹 𝑶 - 𝑩 𝑶 𝑻 *
 `.trimStart(),
   header: '┏━━⊜ *_%category_* ',
   body: '┃⋄ %cmd %isdiamond %isPremium',
   footer: '┗━━━━━━━━⬣\n',
-  after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من سؤال صاحب البوت .رقمه سوف تجده في الأسفل + شارك فيديوهات صاحب البوت تشجيعا له ان كان هذا البوت قد نال إعجابك*\n+212717457920',
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
@@ -136,10 +133,9 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 text: text,
 contextInfo: {
 externalAdReply: {
-title: 'BOBIZA BOT ♥',
-body: "أول بوت واتساب في العالم العربي 💖",
+title: '𝒁 𝑶 𝑹 𝑶 - 𝑩 𝑶 𝑻 ',
+body: "زورو بوت ",
 thumbnailUrl: 'https://telegra.ph/file/07b5eab57cfe77e14979c.jpg',
-sourceUrl: 'https://instagram.com/noureddine_ouafy',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
